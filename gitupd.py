05cfe42bfd480aa09bb42da5820c2490a4210432
chronischeFliegenklatsche <nannuyalrif@gmail.com>
2024-06-25 21:47:42 +0200

    update updating script

--- gitupd.py
+++ gitupd.py
@@ -9,28 +9,20 @@ def clone_if_newer():
         repo = Repo(current_dir)
         
         # Fetch the latest changes from the remote
-        for remote in repo.remotes:
-            remote.fetch()
+        origin = repo.remotes.origin
+        origin.fetch()
         
         # Get the hash of the latest local commit
         local_commit = repo.head.commit.hexsha
         
         # Get the hash of the latest remote commit
-        remote_commit = None
-        for ref in repo.refs:
-            if ref.name == 'origin/HEAD' or ref.name.startswith('origin/') and ref.name.split('/')[-1] == repo.active_branch.name:
-                remote_commit = ref.commit.hexsha
-                break
-        
-        if not remote_commit:
-            print("Unable to determine the latest remote commit. Aborting.")
-            return
+        remote_commit = origin.refs[repo.active_branch.name].commit.hexsha
         
         if local_commit != remote_commit:
             print("Remote repository is newer. Cloning...")
             
             # Get the remote URL
-            remote_url = repo.remotes.origin.url
+            remote_url = origin.url
             
             # Move up one directory
             os.chdir('..')
@@ -42,7 +34,19 @@ def clone_if_newer():
             print(f"Repository cloned to {new_dir}")
             print("Please review the new clone and replace the old directory if desired.")
         else:
-            print("Local repository is up to date. No action needed.")
+            # Check for any differences between local and remote
+            diff = repo.git.diff('HEAD', f'origin/{repo.active_branch.name}')
+            if diff:
+                print("Remote repository has changes. Cloning...")
+                # Perform cloning as before
+                remote_url = origin.url
+                os.chdir('..')
+                new_dir = current_dir + '_new'
+                Repo.clone_from(remote_url, new_dir)
+                print(f"Repository cloned to {new_dir}")
+                print("Please review the new clone and replace the old directory if desired.")
+            else:
+                print("Local repository is up to date. No action needed.")
     
     except git.exc.InvalidGitRepositoryError:
         print("This directory is not a valid Git repository.")
