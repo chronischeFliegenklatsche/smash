3e7581eae09154980a1b198a1e4c3299266d597e
chronischeFliegenklatsche <nanuyalrif@gmail.com>
2024-06-26 18:30:41 +0200

    Start implementing RGB Matrix Rendering API

--- include/smash/RgbMatrixRenderingAPI.hpp
+++ include/smash/RgbMatrixRenderingAPI.hpp
@@ -2,36 +2,10 @@
 #define _RGB_MATRIX_RENDERING_API_HPP
 
 #include "RenderingAPI.hpp"
-#include <Adafruit_Protomatter.h>
+#include <ESP32-HUB75-MatrixPanel-I2S-DMA>
 
 namespace smash
 {
 
-    struct RgbMatrixPinConfiguration;
-    class RgbMatrixRenderingAPI;
-
-    struct RgbMatrixPinConfiguration
-    {
-        uint8_t width;
-        uint8_t bitDepth;
-        uint8_t *rgbPins;
-        uint8_t *addrPins;
-        uint8_t clk;
-        uint8_t lat;
-        uint8_t oe;
-    };
-
-    class RgbMatrixRenderingAPI : public RenderingAPI
-    {
-        mutable Adafruit_Protomatter m_Matrix;
-    public:
-
-        RgbMatrixRenderingAPI(RgbMatrixPinConfiguration cfg);
-        virtual ~RgbMatrixRenderingAPI();
-
-        void drawPixel(int x, int y, uint16_t color) const override;
-        void swapFrameBuffers() const override;
-    };
 }
-
 #endif
