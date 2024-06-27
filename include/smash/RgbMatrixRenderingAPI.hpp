14c2d838b33d7f0db130e939382e2d7c83233b75
chronischeFliegenklatsche <nannuyalrif@gmail.com>
2024-06-27 13:04:29 +0200

    Fix RGBMATRIXRENDERINGAPI

--- include/smash/RgbMatrixRenderingAPI.hpp
+++ include/smash/RgbMatrixRenderingAPI.hpp
@@ -1,11 +1,13 @@
+#ifdef ARDUINO
 #ifndef _RGB_MATRIX_RENDERING_API_HPP
 #define _RGB_MATRIX_RENDERING_API_HPP
 
 #include "RenderingAPI.hpp"
-#include <ESP32-HUB75-MatrixPanel-I2S-DMA>
+#include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
 
 namespace smash
 {
 
 }
+#endif
 #endif
